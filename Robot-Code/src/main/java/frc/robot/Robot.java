35d05fa054bbfc0768383c0366b1e778d0d79ea9
Caroline Joy Pickett <cj@prodical.us>
2025-01-24 17:23:49 -0600

    Robot Run and States Available to be Called

---
+++ Robot-Code/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,97 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.Timer;
+
+public class Robot extends TimedRobot {
+  private Command m_autonomousCommand;
+
+  private RobotContainer m_robotContainer;
+
+  private Timer disabledTimer;
+  // -----------------------------------------------------------------------------------    Controller(s)    ------------------------------------------------------------------------------------
+  final static CommandXboxController operatorDriver = new CommandXboxController(Constants.operatorDriver.port);                                // Driver's xBox controller
+  final static CommandXboxController operatorManip  = new CommandXboxController(Constants.operatorManip.port);                                 // Operator's xBox controller
+  // --------------------------------------------------------------------------------------    Motor(s)    --------------------------------------------------------------------------------------
+
+  // -------------------------------------------------------------------------------------    Sensor(s)    --------------------------------------------------------------------------------------
+
+  // ----------------------------------------------------------------------------------//    Robot Init    //------------------------------------------------------------------------------------
+  @Override
+  public void robotInit() {                                                                                                             // Called on robot startup, best use is for anything needed to prep robot and ensure std. start state
+    m_robotContainer = new RobotContainer();
+    disabledTimer = new Timer();                                                                                                        // Create a timer to disable motor brake a few seconds after disable.  This will let the robot stop immediately when disabled, but then also let it be pushed
+    if (isSimulation()) {                                                                                                               // Checks to see if code is running in simulation
+      DriverStation.silenceJoystickConnectionWarning(true);                                                                     // -> Silences Joystick Connection Warning
+    }
+  }
+  // ---------------------------------------------------------------------------------//    Robot Prdic    //------------------------------------------------------------------------------------
+  @Override
+  public void robotPeriodic() {                                                                                                         // Called every (20ms) in all modes, this is triggered after mode specific periodics
+    CommandScheduler.getInstance().run();
+  }
+  // -----------------------------------------------------------------------------------//    Stop Init    //------------------------------------------------------------------------------------
+  @Override
+  public void disabledInit() {                                                                                                          // Run at moment of start of disable
+    m_robotContainer.setMotorBrake(true);                                                                                           // Sets motor controllers to brake mode on swerve
+    disabledTimer.reset();
+    disabledTimer.start();
+  }
+  // ----------------------------------------------------------------------------------//    Stop Prdic    //------------------------------------------------------------------------------------
+  @Override
+  public void disabledPeriodic() {                                                                                                      // Periodic run during robot disabled state
+
+  }
+  // -----------------------------------------------------------------------------------//    Stop Exit    //------------------------------------------------------------------------------------
+  @Override
+  public void disabledExit() {                                                                                                          // Run on exit of robot disabled state
+
+  }
+  // -----------------------------------------------------------------------------------//    Auto Init    //------------------------------------------------------------------------------------
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+  // ----------------------------------------------------------------------------------//    Auto Prdic    //------------------------------------------------------------------------------------
+  @Override
+  public void autonomousPeriodic() {}
+  // -----------------------------------------------------------------------------------//    Auto Exit    //------------------------------------------------------------------------------------
+  @Override
+  public void autonomousExit() {}
+  // -----------------------------------------------------------------------------------//    Tele Init    //------------------------------------------------------------------------------------
+  @Override
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+  // ----------------------------------------------------------------------------------//    Tele Prdic    //------------------------------------------------------------------------------------
+  @Override
+  public void teleopPeriodic() {}
+  // -----------------------------------------------------------------------------------//    Tele Exit    //------------------------------------------------------------------------------------
+  @Override
+  public void teleopExit() {}
+  // -----------------------------------------------------------------------------------//    Test Init    //------------------------------------------------------------------------------------
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+  // ----------------------------------------------------------------------------------//    Test Prdic    //------------------------------------------------------------------------------------
+  @Override
+  public void testPeriodic() {}
+  // -----------------------------------------------------------------------------------//    Test Exit    //------------------------------------------------------------------------------------
+  @Override
+  public void testExit() {}
+}
---
+++ Robot-Code/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,78 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.subsystems.swervedrive.swerveSubsystem;
+import frc.robot.tools.utils;
+import swervelib.SwerveInputStream;
+
+import java.io.File;
+
+import com.pathplanner.lib.auto.NamedCommands;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class RobotContainer {
+
+  private final swerveSubsystem drivebase = new swerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/"+utils.RoboRIOid()));             // The robot's swerve drive subsystems and commands are defined here
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(                                                                        // Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
+    drivebase.getSwerveDrive(),
+    () -> Robot.operatorDriver.getLeftY() * -1,
+    () -> Robot.operatorDriver.getLeftX() * -1)
+    .withControllerRotationAxis(Robot.operatorDriver::getRightX)
+    .deadband(Constants.operatorManip.RIGHT_X_DEADBAND)
+    .scaleTranslation(0.8)
+    .allianceRelativeControl(true);
+  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(                                           // Clone's the angular velocity input stream and converts it to a fieldRelative input stream.
+    Robot.operatorDriver::getRightX, 
+    Robot.operatorDriver::getRightY)
+    .headingWhile(true);
+  // ----------------------------------------------------------------------------------//    Robot Cntr    //------------------------------------------------------------------------------------
+  public RobotContainer() {                                                                                                             // The container for the robot. Contains subsystems, OI devices, and commands.
+    configureBindings();
+    DriverStation.silenceJoystickConnectionWarning(true);
+    NamedCommands.registerCommand("test", Commands.print("I am alive, I know things!"));
+  }
+  // -----------------------------------------------------------------------------------//    Bindings    //-------------------------------------------------------------------------------------
+  private void configureBindings() {                                                                                                    //  Use this method to define your trigger->command mappings.
+//    Command driveFieldOrientedDirectAngle           = drivebase.driveFieldOriented(driveDirectAngle);
+    Command driveFieldOrientedAngularVelocity       = drivebase.driveFieldOriented(driveAngularVelocity);
+//    Command driveSetpointGen                        = drivebase.driveWithSetpointGeneratorFieldRelative(driveDirectAngle);
+
+    drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+
+    if (DriverStation.isTest()) {                                                                                                       // During Driver Station Test mode set controller binding to
+      drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+
+      Robot.operatorDriver.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+      Robot.operatorDriver.y().whileTrue(drivebase.driveToDistance(1.0, 0.2));
+      Robot.operatorDriver.start().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      Robot.operatorDriver.back().whileTrue(drivebase.modulesCenter());
+      Robot.operatorDriver.leftBumper().onTrue(Commands.none());
+      Robot.operatorDriver.rightBumper().onTrue(Commands.none());
+    } else {                                                                                                                            // During Driver Station Teleop mode set controller binding to
+      Robot.operatorDriver.a().onTrue((Commands.none()));
+      Robot.operatorDriver.b().onTrue((Commands.none()));
+      Robot.operatorDriver.x().onTrue((Commands.none()));
+      Robot.operatorDriver.y().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      Robot.operatorDriver.start().onTrue((Commands.none()));
+      Robot.operatorDriver.back().onTrue((Commands.none()));
+      Robot.operatorDriver.leftBumper().onTrue((Commands.none()));
+      Robot.operatorDriver.rightBumper().onTrue((Commands.none()));
+    }
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+  // -------------------------------------------------------------------------------//    Auto Pass Through    //--------------------------------------------------------------------------------
+
+  public void setMotorBrake(boolean brake) {
+    drivebase.modulesBraking(brake);
+  }
+}
