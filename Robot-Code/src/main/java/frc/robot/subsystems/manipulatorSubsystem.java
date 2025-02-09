a30f80e6cf659e3d0ca7d32d08879552e7a8ab98
Caroline Joy Pickett <cj@prodical.us>
2025-02-08 19:06:33 -0600

    Adding Commands
    - Commands added autoFeederIntake, autoScore, manualElevatorUp, manualElevatorDown, manualScoreIn, manualScoreOut

--- Robot-Code/src/main/java/frc/robot/subsystems/manipulatorSubsystem.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/manipulatorSubsystem.java
@@ -4,16 +4,60 @@ import frc.robot.Robot;
 import com.revrobotics.spark.SparkClosedLoopController;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class manipulatorSubsystem extends SubsystemBase {
-    private SparkClosedLoopController elevatorControl       = Robot.elevatorMotor_Lead.getClosedLoopController();                       // Retrievs Rev MaxMotion Closed Loop Controller
+    private static Timer lastZerod                          = new Timer();                                                              // Time Elapsed Since Last Zero'd
+    //private SparkClosedLoopController elevatorControl       = Robot.elevatorMotor_Lead.getClosedLoopController();                       // Retrievs Rev MaxMotion Closed Loop Controller
     // ------------------------------------------------------------------------------------    Command(s)    ------------------------------------------------------------------------------------
+    public Command autoFeederIntake() {
+        return run(() ->{
+        });
+    }
+    public Command autoScore(int level) {
+        return run(() ->{
+            switch (level) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+    public Command manualElevatorUp() {
+        return run(() ->{
+        });
+    }
+    public Command manualElevatorDown() {
+        return run(() ->{
+        });
+    }
+    public Command manualScoreIn() {
+        return run(() ->{
+        });
+    }
+    public Command manualScoreOut() {
+        return run(() ->{
+        });
+    }
+    // -----------------------------------------------------------------------------------    Periodic(s)    ------------------------------------------------------------------------------------
     public manipulatorSubsystem() {                                                                                                     // Creates Manipulator Subsystem AKA: Initialization
         
     }
-    // -----------------------------------------------------------------------------------    Periodic(s)    ------------------------------------------------------------------------------------
     @Override
     public void periodic () {                                                                                                           // Scheduled every 20ms by Subsystem Base
         dashboardUpdate();
@@ -33,6 +77,13 @@ public class manipulatorSubsystem extends SubsystemBase {
 
     }
     private static void encoderZero() {
+        /*if(Robot.elevatorSensor_3sZero.get()) {
+            Robot.elevatorEncoder_LeadAlt.setPosition(0);
+        } else {
 
+        }*/
+    }
+    private static void runToTarget(double target) {
+        
     }
 }
