e8e8e98139c605869a90c5f5332b7816f2a8b484
Caroline Joy Pickett <cj@prodical.us>
2025-02-14 09:31:48 -0600

    Updated .gitignore for BuildConstants.java

--- Robot-Code/src/main/java/frc/robot/BuildConstants.java
+++ Robot-Code/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 47;
-  public static final String GIT_SHA = "9a9a43d909308d7c30bf005a46e407716659d283";
-  public static final String GIT_DATE = "2025-02-01 23:16:47 CST";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "bd0378478c02a477dd9dd235dfc233db96dd6da6";
+  public static final String GIT_DATE = "2025-02-11 11:05:07 CST";
   public static final String GIT_BRANCH = "Caroline-01/31-ManipulatorSystem";
-  public static final String BUILD_DATE = "2025-02-02 10:54:39 CST";
-  public static final long BUILD_UNIX_TIME = 1738515279574L;
+  public static final String BUILD_DATE = "2025-02-12 18:32:34 CST";
+  public static final long BUILD_UNIX_TIME = 1739406754055L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
