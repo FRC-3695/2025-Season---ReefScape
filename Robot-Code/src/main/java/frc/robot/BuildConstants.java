05e10430be1191b32db021106ec9ee66f3782604
Chris N <2707036@y115.org>
2025-03-11 15:58:40 -0500

    Fixed Gradle

--- Robot-Code/src/main/java/frc/robot/BuildConstants.java
+++ Robot-Code/src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 77;
-  public static final String GIT_SHA = "f3887be228e422ac9b1a89cf51e2be7d7acf5276";
-  public static final String GIT_DATE = "2025-03-11 14:05:26 CDT";
-  public static final String GIT_BRANCH = "Caroline-01/31-ManipulatorSystem";
-  public static final String BUILD_DATE = "2025-03-11 15:37:28 CDT";
-  public static final long BUILD_UNIX_TIME = 1741725448814L;
+  public static final int GIT_REVISION = 86;
+  public static final String GIT_SHA = "af8972aa62e897be5ce760b0f15130c8ec476a76";
+  public static final String GIT_DATE = "2025-03-11 15:55:05 CDT";
+  public static final String GIT_BRANCH = "Chris-03/11-Algae";
+  public static final String BUILD_DATE = "2025-03-11 15:58:14 CDT";
+  public static final long BUILD_UNIX_TIME = 1741726694165L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
