ebcb3e8cf68cb35b8f1836dc150366d06c9bc8f9
Chris N <2707036@y115.org>
2025-03-10 19:35:11 -0500

    Climber Motor Functionality
    I know we're not doing climb, but I can swap the motors out with other ones and it should work relatively the same.  I'll also add limit switch stuff when we have DIOs. This probably doesn't work yet.

--- Robot-Code/src/main/java/frc/robot/subsystems/manipulatorSubsystem.java
+++ Robot-Code/src/main/java/frc/robot/subsystems/manipulatorSubsystem.java
@@ -1,12 +1,24 @@
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
 import frc.robot.Robot;
 
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+
+
+
+
 public class manipulatorSubsystem extends SubsystemBase {
-   
 
-//
 
-    
+    static CommandXboxController operatorDriver = new CommandXboxController(Constants.operatorDriver.port);
+    private final SparkFlex climber_Paw = new SparkFlex(Constants.CANnet.climber.Paw, MotorType.kBrushless);
+
+    public void periodic() {
+        double speed = operatorDriver.getLeftY(); 
+        climber_Paw.set(speed);
+    }
 }
