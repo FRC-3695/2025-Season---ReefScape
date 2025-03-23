8e9272a8e0afee4928c12952ba03d1de067f9680
ChrisNendick <2707036@y115.org>
2025-03-23 15:20:22 -0500

    Algae arm conversion factor

--- Robot-Code/src/main/java/frc/robot/Constants.java
+++ Robot-Code/src/main/java/frc/robot/Constants.java
@@ -148,6 +148,9 @@ public class Constants {
             public static final int         stallAmp        = 40;                                                                           // Stall Amps
             public static final double      autoFeed        = -00.50;
             public static final double      autoEject       = 00.50;
+            public static final double      algaeArmRatio   = 1;                                                                            // 
+            public static final double      algaeArmCal     = 1;   
+            public static final double      algaeConversionFact   = (((90 / 360.0) * 100) / ratioGearBox) * algaeArmRatio * algaeArmCal;    // Calculation for the conversion factor of the Algae Arm
         }
         public static final class visionPosition {
 
